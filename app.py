6feba8fc8cd41a108381cb95ef776eba500c58df
Alicja <amalinowska.p@gmail.com>
2020-04-13 14:28:14 +0100

    adding user to database with password hashed implemented

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bson.objectid import ObjectId
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from registration import RegistrationForm
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 app.secret_key = b'K/\x81\xc6\xa0R%k[mSm\xfe\xc6\x8a\xa7'#REMEMBER TO REMOVE BEFORE DEPLOYMENT
@@ -13,13 +14,22 @@ app.config["MONGO_URI"] = MONGODB_URI
 
 mongo = PyMongo(app)
 Bootstrap(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
+#login_manager = LoginManager()
+#login_manager.init_app(app.py)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def registration():
-    
-    return 'dupa'
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate_on_submit():
+        users = mongo.db.users
+        profile = request.form.to_dict()
+        profile["confirm"] = sha256_crypt.hash(profile["confirm"])
+        profile["password"] = sha256_crypt.hash(profile["password"])
+        users.insert_one(profile)
+        return "All done! You're registered"
+
+    return render_template('registration.html', form=form)
+
 
 
 if __name__ == '__main__':
