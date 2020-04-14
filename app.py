3752233e8c9739d023a057216aa39314ec6d6c21
Alicja <amalinowska.p@gmail.com>
2020-04-14 15:33:15 +0100

    login implemented

--- app.py
+++ app.py
@@ -3,21 +3,81 @@ from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
-from registration import RegistrationForm
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from forms import RegistrationForm, LoginForm
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
-app.secret_key = b'K/\x81\xc6\xa0R%k[mSm\xfe\xc6\x8a\xa7'#REMEMBER TO REMOVE BEFORE DEPLOYMENT
-MONGODB_URI = os.getenv("MONGO_URI").replace('"', '')#REMEMBER TO REMOVE REPLACE BEFORE DEPLOYMENT
+# REMEMBER TO REMOVE BEFORE DEPLOYMENT
+app.secret_key = b'K/\x81\xc6\xa0R%k[mSm\xfe\xc6\x8a\xa7'
+# REMEMBER TO REMOVE REPLACE BEFORE DEPLOYMENT
+MONGODB_URI = os.getenv("MONGO_URI").replace('"', '')
 app.config["MONGO_URI"] = MONGODB_URI
 
 mongo = PyMongo(app)
 Bootstrap(app)
-#login_manager = LoginManager()
-#login_manager.init_app(app.py)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route('/', methods=['GET', 'POST'])
+
+class User(UserMixin):
+    def __init__(self, email, id, first_name):
+        self.email = email
+        self.id = id
+        self.first_name = first_name
+
+    def get_id(self):
+        return self.email
+
+    @staticmethod
+    def validate_login(password, hash):
+        return sha256_crypt.verify(password, hash)
+
+
+
+'''@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate_on_submit():
+        user = mongo.db.users.find_one(
+            {"email_address": form.email_address.data})
+        if user and User.validate_login(form.password.data, user['password']):
+            user_obj = User(user['email_address'],user['_id'], user['first_name'])
+            login_user(user_obj)
+            print(user_obj)
+            return redirect(url_for('index'))
+        flash("Wrong username or password!", category='error')
+    return render_template('login.html', title='login', form=form)'''
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        user = mongo.db.users.find_one({"email_address": form.email_address.data})
+        if user and User.validate_login(form.password.data, user['password']):
+            user_obj = User(user['email_address'], user['_id'], user['first_name'])
+            print(current_user.is_authenticated)
+            login_user(user_obj)
+            print(current_user.is_authenticated)
+            flash("Logged in successfully!", category='success')
+            return redirect(request.args.get("next") or url_for("index"))
+        flash("Wrong username or password!", category='error')
+    return render_template('login.html', title='login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate_on_submit():
@@ -34,9 +94,22 @@ def registration():
 
     return render_template('registration.html', form=form)
 
-users = mongo.db.users.count_documents({"email_address": "amalinowska.p@gmail.com"})
-print(users)
 
+@app.route('/planner')
+@login_required
+def planner():
+    return render_template('planner.html')
+
+
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(email):
+    user = mongo.db.users.find_one({"email_address": email})
+    if not user:
+        return None
+    return User(user['email_address'], user['_id'], user['first_name'])
 
 
 if __name__ == '__main__':
