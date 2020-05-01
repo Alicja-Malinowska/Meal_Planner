2aea0fae039b65c6916b8804d99c801e6b148c8e
Alicja <amalinowska.p@gmail.com>
2020-05-01 17:30:52 +0100

    port and ip added

--- app.py
+++ app.py
@@ -292,5 +292,5 @@ def load_user(email):
     return User(user['email_address'], user['_id'], user['first_name'])
 
 
-if __name__ == '__main__':
-    app.run()
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=True)
