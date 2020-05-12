5e9aa22229f90bfa669dad9842926a84fc2431e5
Alicja <amalinowska.p@gmail.com>
2020-05-12 15:31:09 +0100

    clear button removed from datepicker

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,7 +2,7 @@ $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
     today: 'Today',
-    clear: 'Clear',
+    clear: false,
     close: 'Close',
     closeOnSelect: true, // Close upon selecting a date,
     container: undefined, // ex. 'body' will append picker to body
