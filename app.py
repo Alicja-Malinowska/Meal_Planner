fce551ab94c1bf2c4347158da47e12087dc3abce
Alicja <amalinowska.p@gmail.com>
2020-05-15 14:10:14 +0100

    comments added

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from urllib.parse import urlparse, urljoin
 app = Flask(__name__)
 
 MONGODB_URI = os.getenv("MONGO_URI").replace('"', '')
-app.secret_key = b'K/\x81\xc6\xa0R%k[mSm\xfe\xc6\x8a\xa7'
+app.secret_key = SECRET_KEY
 app.config["MONGO_URI"] = MONGODB_URI
 
 
@@ -40,12 +40,15 @@ TODAY = DATE_INST.today()
 
 #this snippet is taken from stack overflow as the link in the offical Flask documentation doesn't work
 def is_safe_url(target):
+    '''this is used to check if the url is dafe before redirecting to "next"'''
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ('http', 'https') and \
            ref_url.netloc == test_url.netloc
 
 def get_week(start_date):
+    '''given a start date in datetime format, returns an array of 7 tuples
+    each tuple includes a day and day of the week'''
     the_week = [(start_date, WEEK_DAYS[str(start_date.weekday())])]
     for i in range(1,7):
         next_day = start_date + datetime.timedelta(days=i)
@@ -54,6 +57,8 @@ def get_week(start_date):
 
 
 def get_week_recipes(current_week):
+    '''makes a call to the database to find all the recipes scheduled in a given week
+    takes current_week as an argument, which should be return from get_week function'''
     recipes = mongo.db.recipes
     week_dates = []
     for day_tuple in current_week:
@@ -71,6 +76,7 @@ def get_week_recipes(current_week):
     return week_recipes_data
 
 def get_tags(all_recipes):
+    '''gathers tags from the recipes given, without duplicates, sorted alphabetically'''
     all_tags = []
     for recipe in all_recipes:
         tags = recipe["tags"]
@@ -79,8 +85,31 @@ def get_tags(all_recipes):
     tag_set = sorted(set(all_tags))
     return tag_set
 
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    ''' adds a user to database if a user with given email does not already exists'''
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate_on_submit():
+        users = mongo.db.users
+        profile = request.form.to_dict()
+        if users.count_documents({"email_address": profile["email_address"]}) > 0:
+            flash("An account has already been registered for this email address", "errors")
+            return render_template('registration.html', form=form)
+        else:
+            profile["confirm"] = sha256_crypt.hash(profile["confirm"])
+            profile["password"] = sha256_crypt.hash(profile["password"])
+            users.insert_one(profile)
+            user_obj = User(profile['email_address'], profile['first_name'])
+            login_user(user_obj)
+            flash("All done! You're registered!", "success")
+            return redirect(url_for('home'))
+
+    return render_template('registration.html', form=form)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    ''' logs user in if email address and password match'''
     form = LoginForm()
     if request.method == 'POST' and form.validate_on_submit():
         user = mongo.db.users.find_one({"email_address": form.email_address.data})
@@ -104,6 +133,7 @@ def logout():
 
 @app.route('/')
 def home():
+    ''' renders home view, gets scheduled recipes from database for logged in user'''
     recipes = mongo.db.recipes
     today = TODAY.strftime("%Y-%m-%d")
     today_recipes = []
@@ -113,27 +143,6 @@ def home():
         if recipes.count_documents({'dates':{'$elemMatch':{'$elemMatch':{'$in':[today]}}}, 'owner': current_user.email}) == 0:
             message = "You have nothing scheduled today."
     return render_template('home.html', today_recipes=today_recipes, message=message)
-
-
-@app.route('/registration', methods=['GET', 'POST'])
-def registration():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate_on_submit():
-        users = mongo.db.users
-        profile = request.form.to_dict()
-        if users.count_documents({"email_address": profile["email_address"]}) > 0:
-            flash("An account has already been registered for this email address", "errors")
-            return render_template('registration.html', form=form)
-        else:
-            profile["confirm"] = sha256_crypt.hash(profile["confirm"])
-            profile["password"] = sha256_crypt.hash(profile["password"])
-            users.insert_one(profile)
-            user_obj = User(profile['email_address'], profile['first_name'])
-            login_user(user_obj)
-            flash("All done! You're registered!", "success")
-            return redirect(url_for('home'))
-
-    return render_template('registration.html', form=form)
 
 
 
@@ -148,17 +157,19 @@ def planner():
 @app.route('/planner/next', methods=['GET', 'POST'])
 @login_required
 def next():
+    ''' calculates dates for +1 week from last view'''
     first_week_day = request.form.get('first_week_day')
     date_list = first_week_day.split('-')
     date_obj = datetime.date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
     current_week = get_week(date_obj + datetime.timedelta(weeks=1))
     first_week_day = str(current_week[0][0])
     week_recipes = get_week_recipes(current_week)
-    return render_template('planner.html', current_week=current_week, first_week_day=first_week_day, week_recipes=week_recipes)#morning_recipes=week_recipes[0], afternoon_recipes=week_recipes[1], evening_recipes=week_recipes[2])
+    return render_template('planner.html', current_week=current_week, first_week_day=first_week_day, week_recipes=week_recipes)
 
 @app.route('/planner/previous', methods=['GET', 'POST'])
 @login_required
 def previous():
+    ''' calculates dates for -1 week from last view'''
     first_week_day = request.form.get('first_week_day')
     date_list = first_week_day.split('-')
     date_obj = datetime.date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
@@ -170,6 +181,7 @@ def previous():
 @app.route('/planner/jump_to')
 @login_required
 def jump_to():
+    ''' renders a view of week starting with a date chosen by user'''
     selected_date = request.args.get('jump_to')
     date_list = selected_date.split('-')
     first_week_date = datetime.date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
@@ -180,6 +192,7 @@ def jump_to():
 @app.route('/recipes')
 @login_required
 def recipes():
+    ''' gets all user's recipes from database'''
     form = SearchName()
     recipes = mongo.db.recipes
     all_recipes = recipes.find({'owner': current_user.email})
@@ -191,6 +204,7 @@ def recipes():
 @app.route('/recipes/add', methods=['POST', 'GET'])
 @login_required
 def add_recipe():
+    ''' created a new recipe document in database with user's data'''
     form = AddRecipe()
     target = os.path.join(APP_ROOT, 'static/images/recipe-images/')  #folder path
     if not os.path.isdir(target):
@@ -200,10 +214,10 @@ def add_recipe():
         new_recipe = request.form.to_dict()
         image = request.files[form.image.name]
         
-        if image.filename:
+        if image.filename: # if image was uploaded
             name, file_extension = os.path.splitext(image.filename)
-            filename = secure_filename(str(uuid.uuid1()) + file_extension)
-            destination = "".join([target, filename])
+            filename = secure_filename(str(uuid.uuid1()) + file_extension) # get a unique filename
+            destination = "".join([target, filename]) #create path to save the file
             image.save(destination)
         else: 
             filename = 'default.png'
@@ -222,8 +236,9 @@ def add_recipe():
 @app.route('/show_recipe/<recipe_id>')
 @login_required
 def show_recipe(recipe_id):
+    ''' gets a demanded recipe from database '''
     recipes = mongo.db.recipes
-    if ObjectId.is_valid(recipe_id) and recipes.count_documents({'_id': ObjectId(recipe_id), 'owner': current_user.email}) > 0:
+    if ObjectId.is_valid(recipe_id) and recipes.count_documents({'_id': ObjectId(recipe_id), 'owner': current_user.email}) > 0: #the validity check is needed in case user tries to type the url themselves
         recipe = recipes.find({'_id': ObjectId(recipe_id), 'owner': current_user.email})
     else:
         return abort(404)
@@ -232,6 +247,7 @@ def show_recipe(recipe_id):
 @app.route('/recipes/delete', methods=['POST'])
 @login_required
 def delete_recipe():
+    ''' removes recipe document from database '''
     recipe_id = request.form.get('recipe_id')
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     flash('Recipe deleted!', 'success')
@@ -241,6 +257,7 @@ def delete_recipe():
 @app.route('/recipes/edit/<recipe_id>', methods=['GET','POST'])
 @login_required
 def edit_recipe(recipe_id):
+    ''' saves a recipe with user changes in the database '''
     form = AddRecipe()
     the_recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     target = os.path.join(APP_ROOT, 'static/images/recipe-images/')
@@ -251,7 +268,7 @@ def edit_recipe(recipe_id):
         instructions = request.form.get('instructions').splitlines(True)
         tags = request.form.get('tags').lower().replace(" ", "").strip(";").split(";")
         image = request.files[form.image.name]
-        if image:
+        if image: #if a new file was uploaded
             file_name, file_extension = os.path.splitext(image.filename)
             filename = secure_filename(str(uuid.uuid1()) + file_extension)
             destination = "".join([target, filename])
@@ -277,6 +294,7 @@ def edit_recipe(recipe_id):
 @app.route('/schedule', methods=['POST'])
 @login_required
 def schedule():
+    ''' adds chosen date to dates array in the recipe document '''
     recipes = mongo.db.recipes
     recipe_id = request.form.get('recipe_id')
     date = request.form.get('schedule_date')
@@ -288,6 +306,7 @@ def schedule():
 @app.route('/del_from_schedule/<recipe_id>/<date>/<daytime>/<first_week_day>')
 @login_required
 def del_from_schedule(recipe_id, date, daytime, first_week_day):
+    ''' removes date from dates array in the recipe document '''
     recipes = mongo.db.recipes
     recipes.update( {'_id': ObjectId(recipe_id)}, {"$pull": {"dates": (date, daytime)}})
     date_list = first_week_day.split('-')
@@ -300,6 +319,7 @@ def del_from_schedule(recipe_id, date, daytime, first_week_day):
 @app.route('/search_name',  methods=['GET','POST'])
 @login_required
 def search_name():
+    ''' performs a case insensitive search for user's recipe documents including searched string '''
     form = SearchName()
     recipes = mongo.db.recipes
     all_recipes = recipes.find({'owner': current_user.email})
@@ -318,6 +338,7 @@ def search_name():
 @app.route('/search_tag')
 @login_required
 def search_tag():
+    ''' retrievs all user's recipes with selected tag '''
     form = SearchName()
     recipes = mongo.db.recipes
     all_recipes = recipes.find({'owner': current_user.email})
@@ -332,6 +353,7 @@ def search_tag():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+# view to be loaded if user is not logged in and login is required to access a page
 login_manager.login_view = 'login'
 
 
@@ -344,4 +366,4 @@ def load_user(email):
 
 
 app.run(host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "5000")), debug=True)
+        port=int(os.getenv("PORT", "5000")))
